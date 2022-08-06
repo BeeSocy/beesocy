437b23a01fcc4934386f5cb696d932a9d7dd2f68
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-06 08:40:55 -0300

    Change at theme styles;

--- front/src/styles/global.ts
+++ front/src/styles/global.ts
@@ -6,22 +6,25 @@ export default createGlobalStyle`
     }
     
     body {
-        background: ${props => props.theme.colors.primary};
+        background: ${props => props.theme.colors.secondary};
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         text-rendering: optimizeLegibility;
         -webkit-font-smoothing: antialiased;
     }
     
     * {
-        box-sizing: border-box;
         margin: 0;
         padding: 0;
+        
+        box-sizing: border-box;
         border: none;
         outline: none;
         
-        text-decoration: none;
         color: ${props => props.theme.colors.text};
+        text-decoration: none;
         list-style: none;
+
+        transition: background 0.5s, color 0.5s;
 
         -webkit-touch-callout: none;               
         -webkit-text-size-adjust: none;          
