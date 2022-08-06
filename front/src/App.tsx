44f1906ed8b2df11f523efe12ce92585d420161f
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-06 11:00:22 -0300

    Change theme function;

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,36 @@
 import { useLocalStorage } from 'react-use';
 
 import dark from './styles/themes/dark';
+import light from './styles/themes/light';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
 
+import { IconContext } from 'react-icons';
+import { Header } from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+
 export function App() {
   const defaultTheme = dark;
-  const [theme] = useLocalStorage('theme', defaultTheme);
+
+  const [theme, setTheme] = useLocalStorage('theme', defaultTheme);
+
+  function handleChangeTheme() {
+    setTheme(theme?.title === 'light' ? dark : light);
+  }
 
   return (
     <ThemeProvider theme={theme ? theme : defaultTheme}>
-      <h1>Bom dia mundo</h1>
+      <BrowserRouter>
+        <IconContext.Provider value={{ size: '24' }}>
+          <Header
+            handleChangeTheme={handleChangeTheme}
+            themeTitle={theme?.title ? theme?.title : 'dark'}
+          />
+        </IconContext.Provider>
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
