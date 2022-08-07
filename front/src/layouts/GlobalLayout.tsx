ae1f30d01332087eb343ffcb62b200a30f79317f
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-07 08:55:02 -0300

    Add mobile menu on global layout

--- front/src/layouts/GlobalLayout.tsx
+++ front/src/layouts/GlobalLayout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { MobileMenu } from '../components/MobileMenu';
+import { useMobile } from '../hooks/useMobile';
 
 export function GlobalLayout() {
+  const { isMobile } = useMobile();
   return (
     <>
       <Header />
-      <Menu />
+      {isMobile ? <MobileMenu /> : <Menu />}
       <Outlet />
     </>
   );
