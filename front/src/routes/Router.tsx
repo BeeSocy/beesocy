4f310e480273e596abdd31002d2528c0b716973a
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-06 22:34:34 -0300

    Exclude home and global routes

--- front/src/routes/Router.tsx
+++ front/src/routes/Router.tsx
@@ -7,8 +7,6 @@ export function Router() {
     <Routes>
       <Route path="/" element={<GlobalLayout />}>
         <Route path="/" element={<GlobalFeed />} />
-        <Route path="/home" element={<GlobalFeed />} />
-        <Route path="/global" element={<GlobalFeed />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
