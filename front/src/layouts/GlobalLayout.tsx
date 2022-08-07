e307ef00c37fa1547726f3cd4f6d60fdc34ecd84
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-06 22:35:52 -0300

    Add side menu on global layout

--- front/src/layouts/GlobalLayout.tsx
+++ front/src/layouts/GlobalLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
 
 export function GlobalLayout() {
   return (
     <>
       <Header />
+      <Menu />
       <Outlet />
     </>
   );
