90f462ef37916642b7d5e685e312011ff1844eb8
--global <--global>
2022-10-07 09:42:34 -0300

    change chat

--- src/components/Chat/ChatDesktop/ChatSection/ChatSectionGeneral/styles.ts
+++ src/components/Chat/ChatDesktop/ChatSection/ChatSectionGeneral/styles.ts
@@ -4,7 +4,6 @@ export const ChatSectionStyled = styled.section`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
-  overflow-y: hidden;
   height: 100%;
   background: ${props => props.theme.colors.primary};
 `;
