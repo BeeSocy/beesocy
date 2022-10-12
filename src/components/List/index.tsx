9a2030d4e36ce17a678ba21bdf102213a33b109a
santosfabin <fsfabianosanto03@gmail.com>
2022-10-12 10:26:11 -0300

    Create list content

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,25 +1,73 @@
+import { useState } from 'react';
+import { IImagePost } from '../../types/imagePost';
+import { IMusicPost } from '../../types/musicPost';
+import { ImageCard } from '../Cards/ImageCard';
+import { MusicCard } from '../Cards/MusicCard';
 import {
   BreadCrump,
   BreadCrumpContainer,
   Container,
   Header,
+  ImageContent,
+  MusicContent,
   Title
 } from './styles';
 
 interface IListProps {
   title: string;
+  musics: IMusicPost[];
+  images: IImagePost[];
 }
-export function List({ title }: IListProps) {
+export function List({ title, musics, images }: IListProps) {
+  const [activeCategory, setActiveCategory] = useState<
+    'music' | 'image' | 'code'
+  >('music');
+
   return (
     <Container>
       <Header>
         <Title>{title}</Title>
         <BreadCrumpContainer>
-          <BreadCrump>Música</BreadCrump>
-          <BreadCrump>Imagens</BreadCrump>
-          <BreadCrump>Códigos</BreadCrump>
+          <BreadCrump
+            active={activeCategory === 'music'}
+            onClick={() => setActiveCategory('music')}
+          >
+            Músicas
+          </BreadCrump>
+          <BreadCrump
+            active={activeCategory === 'image'}
+            onClick={() => setActiveCategory('image')}
+          >
+            Imagens
+          </BreadCrump>
+          <BreadCrump
+            active={activeCategory === 'code'}
+            onClick={() => setActiveCategory('code')}
+          >
+            Códigos
+          </BreadCrump>
         </BreadCrumpContainer>
       </Header>
+
+      {activeCategory === 'music' && (
+        <MusicContent>
+          {musics.map(value => (
+            <>
+              <MusicCard large {...value} />
+            </>
+          ))}
+        </MusicContent>
+      )}
+
+      {activeCategory === 'image' && (
+        <ImageContent>
+          {images.map(value => (
+            <>
+              <ImageCard {...value} />
+            </>
+          ))}
+        </ImageContent>
+      )}
     </Container>
   );
 }
--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { breakpoint } from "../../hooks/useMobile";
 
 export const Container = styled.div`
@@ -42,16 +42,50 @@ export const BreadCrumpContainer = styled.div`
     flex-wrap: wrap;
 
     gap: 2rem;
+    margin-bottom: 4rem;
 
     @media(max-width: ${breakpoint}) {
         margin-top: 4.8rem;
     }
 `;
 
-export const BreadCrump = styled.span`
+interface IBreadCrumpProps {
+    active?: boolean;
+}
+
+export const BreadCrump = styled.span<IBreadCrumpProps>`
     padding: 0.4rem 2rem;
 
     border: 0.02rem solid ${props => props.theme.colors.secondaryLight};
     border-radius: 2rem;
+
+    &:hover{
+        cursor: pointer;
+    }
+
+    ${props => props.active && css`
+    /* colocar a cor que o cara escolheu aqui */
+        color: ${props.theme.colors.bee};
+        border-color: ${props.theme.colors.bee};
+    `}
+    user-select: none;
+`;
+
+export const MusicContent = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    gap: 2rem;
+`;
+
+export const ImageContent = styled.div`
+    display: grid;
+    grid-auto-flow: row dense;
+    grid-template-columns: repeat(auto-fit, minmax(26.4rem, 1fr));
+
+    justify-content: center;
+
+    gap: 2rem;
 
 `;
